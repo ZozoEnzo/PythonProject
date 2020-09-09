b9bb24d03e49d7dfc07bd8e4e8ac2217b0bd802e
Enzo <enzo.borges@hotmail.fr>
2020-09-09 18:14:47 +0200

    Update method to return good json

--- index.py
+++ index.py
@@ -21,18 +21,52 @@ def lasttonew():
 
 @app.route('/lastandnewpopulation')
 def lastandnewpopulation():
-    results = []
+    #Calcul du nombre d'entré en fonction des années
     datalast = call(URL_YEAR_CINEMA + "1985")
+    data_list_last = json.dumps(datalast['records'][0]['fields'])
+    alls_last = json.loads(data_list_last)
+    list_key_last = list(alls_last.keys())
+    list_val_last = list(alls_last.values())
+    value_last = list_val_last[list_key_last.index('entrees_millions')]
     datanew = call(URL_YEAR_CINEMA + "2019")
+    data_list_new = json.dumps(datanew['records'][0]['fields'])
+    alls_new = json.loads(data_list_new)
+    list_key_new = list(alls_new.keys())
+    list_val_new = list(alls_new.values())
+    value_new = list_val_new[list_key_new.index('entrees_millions')]
+    value_label = 'Million d\'entrée'
+
+    #Calcule de l'évolution
+    evolution = round(((value_new - value_last)/value_last)*100,2)
+    if value_last < value_new:
+        value_evolve = str(evolution) + ' % entrées'
+    else: 
+        value_evolve = '-'+ str(evolution) +' % entrées'
+    
+    #Calcule des populations en fonction des années
     populations = call(URL_POPULATION)
-    results.append(json.dumps(datalast['records'][0]['fields']))
-    results.append(json.dumps(datanew['records'][0]['fields']))
-    for population in populations:
-        if "1985" in population:
-            results.append(json.dumps(population))
-        if "2019" in population: 
-           results.append(json.dumps(population))
+    for item in populations: 
+        if '1985' in item:
+            last_pop = int(item['1985'].replace(' ',''))
+        if '2019' in item:
+            new_pop = int(item['2019'].replace(' ',''))
+    #Calcule de l'évolution de la population en fonction des années
+    evolution_pop = round((((new_pop - last_pop)/last_pop)*100), 2)
+    if last_pop < new_pop:
+        value_evolve_pop = str(evolution_pop) + ' % de population'
+    else: 
+        value_evolve_pop = '-'+ str(evolution_pop)+' % de population'
+    
+    results = { "title" : "Entrée en fonction de la population",
+        "chart": {
+        "data":[{
+        "value" : [value_last, value_new],
+        "label" : value_label
+    }],
+    "labels": ["1985","2019"]},
+    "data": [value_evolve,value_evolve_pop]}
     return json.dumps(results)
+
 
 
 @app.route('/fivemorelessfive/<path:year>')
@@ -46,12 +80,17 @@ def fivemorelessfive(year):
     results.append(json.dumps(dataplusfive['records'][0]['fields']))
     return json.dumps(results)
 
-
 @app.route('/year/<path:year>')
 def index(year):
+    result = []
     url = URL_YEAR_CINEMA + year
     data = call(url)
-    return json.dumps(data['records'][0]['fields'])
+    data_list = json.dumps(data['records'][0]['fields'])
+    alls = json.loads(data_list)
+    list_key = list(alls.keys())
+    list_val = list(alls.values())
+    value = list_val[list_key.index('entrees_millions')]
+    return json.dumps(value)
 
 
 def call(url):
