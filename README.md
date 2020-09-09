ebbfb8880fb61415386ccb132a229964107c0268
Enzo <enzo.borges@hotmail.fr>
2020-09-09 11:52:28 +0200

    Update README.md and index.py

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # PythonProject
+
+###But
+Ce projet va permettre d'afficher des informations concernant le monde du cinéma au cours des années. 
+<br>Nous avons comme différentes informations : 
+- L'année évidemment
+- Le nombre d'entrée, en millions. 
+- Le nombre de séance, en milliers. 
+- La recette réalisé au gichet
+- La recette moyenne par entre en €
--- index.py
+++ index.py
@@ -5,44 +5,16 @@ import numpy as np
 
 app = Flask(__name__)
 
-#@app.route('/year/<path:year>')
-#def index(year):
-#    url = "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=frequentation-dans-les-salles-de-cinema&q=&facet=annee&refine.annee="+year
-#    response = requests.get(url)
-#    tous = json.loads(response.text)
-#    records = json.dumps(tous['records'])
-#    posstart = records.find("fields")
-#    posend = records.find("record_timestamp")
-#    fields = records[posstart+10:posend-4]
-#    if posstart == -1:
-#        abort(404)
-#    return fields
-
-@app.route('/lasttonew')
+@app.route('/lastandnew')
 def lasttoroute():
-    result =  {}
+    datas =  []
     url = "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=frequentation-dans-les-salles-de-cinema&q=&facet=annee&refine.annee=1985"
-    response = requests.get(url)
-    tous = json.loads(response.text)
-    record = tous.get('records')
-    records = json.dumps(tous['records'])
-    posstart = records.find("fields")
-    posend = records.find("record_timestamp")
-    fields = records[posstart+10:posend-4].split(',')
-
     url2 = "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=frequentation-dans-les-salles-de-cinema&q=&facet=annee&refine.annee=2019"
-    response2 = requests.get(url2)
-    tous2 = json.loads(response2.text)
-    records2 = json.dumps(tous2['records'])
-    posstart2 = records2.find("fields")
-    posend2 = records2.find("record_timestamp")
-    fields2 = records2[posstart+10:posend-4].split(',')
-
-    result['1985'] = fields
-    result['2019'] = fields2
-    if posstart == -1:
-        abort(404)
-    print(result)
+    data = call(url)
+    data1 = call(url2)
+    datas.append(json.dumps(data['records'][0]['fields']))
+    datas.append(json.dumps(data1['records'][0]['fields']))
+    return json.dumps(datas)
 
 @app.route('/fivemorelessfive/<path:year>')
 def fivemorelessfive(year):
