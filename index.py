1ff59d9097ac8f89e1e6a8b11ef8b0d4ea1e821b
bellatalex <bellata.alex@gmail.com>
2020-09-09 15:13:45 +0200

    modif

--- index.py
+++ index.py
@@ -5,34 +5,48 @@ import numpy as np
 
 app = Flask(__name__)
 
+URL_YEAR_CINEMA = "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=frequentation-dans-les-salles-de-cinema&q=&facet=annee&refine.annee="
+URL_POPULATION = "http://open-api.green-walk.fr/data.json"
+
+
 @app.route('/lastandnew')
-def lasttoroute():
-    datas =  []
-    url = "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=frequentation-dans-les-salles-de-cinema&q=&facet=annee&refine.annee=1985"
-    url2 = "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=frequentation-dans-les-salles-de-cinema&q=&facet=annee&refine.annee=2019"
-    data = call(url)
-    data1 = call(url2)
-    datas.append(json.dumps(data['records'][0]['fields']))
-    datas.append(json.dumps(data1['records'][0]['fields']))
-    return json.dumps(datas)
+def lasttonew():
+    results = []
+    datalast = call(URL_YEAR_CINEMA + "1985")
+    datanew = call(URL_YEAR_CINEMA + "2019")
+    results.append(json.dumps(datalast['records'][0]['fields']))
+    results.append(json.dumps(datanew['records'][0]['fields']))
+    return json.dumps(results)
+
+
+@app.route('/lastandnewpopulation')
+def lastandnewpopulation():
+    results = []
+    datalast = call(URL_YEAR_CINEMA + "1985")
+    datanew = call(URL_YEAR_CINEMA + "2019")
+    population = call(URL_POPULATION)
+    results.append(json.dumps(datalast['records'][0]['fields']))
+    results.append(json.dumps(datanew['records'][0]['fields']))
+    print(population)
+    #results.append(json.dumps(population))
+    return json.dumps(results)
+
 
 @app.route('/fivemorelessfive/<path:year>')
 def fivemorelessfive(year):
-    datas = []
-    url = "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=frequentation-dans-les-salles-de-cinema&q=&facet=annee&refine.annee="+ str(int(year)-5)
-    url1 = "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=frequentation-dans-les-salles-de-cinema&q=&facet=annee&refine.annee="+ str(int(year))
-    url2 = "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=frequentation-dans-les-salles-de-cinema&q=&facet=annee&refine.annee="+ str(int(year)+5)
-    data = call(url)
-    data1 = call(url1)
-    data2 = call(url2)
-    datas.append(json.dumps(data['records'][0]['fields']))
-    datas.append(json.dumps(data1['records'][0]['fields']))
-    datas.append(json.dumps(data2['records'][0]['fields']))
-    return json.dumps(datas)
+    results = []
+    dataminusfive = call(URL_YEAR_CINEMA + str(int(year)-5))
+    data = call(URL_YEAR_CINEMA + str(int(year)))
+    dataplusfive = call(URL_YEAR_CINEMA + str(int(year)+5))
+    results.append(json.dumps(dataminusfive['records'][0]['fields']))
+    results.append(json.dumps(data['records'][0]['fields']))
+    results.append(json.dumps(dataplusfive['records'][0]['fields']))
+    return json.dumps(results)
+
 
 @app.route('/year/<path:year>')
 def index(year):
-    url = "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=frequentation-dans-les-salles-de-cinema&q=&facet=annee&refine.annee=" + year
+    url = URL_YEAR_CINEMA + year
     data = call(url)
     return json.dumps(data['records'][0]['fields'])
 
@@ -40,9 +54,10 @@ def index(year):
 def call(url):
     response = requests.get(url)
     data = response.json()
-    if data['nhits'] < 1:
-        abort(404)
+    #if data['nhits'] < 1:
+    #   abort(404)
     return data
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000, host='0.0.0.0')
