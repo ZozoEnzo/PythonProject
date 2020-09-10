8affda067c7cfac7adb4f4d3501783adfdf90bb3
Enzo <enzo.borges@hotmail.fr>
2020-09-10 11:46:42 +0200

    Ajout de fonctionnalité pour permettre le calcul -5 +5

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # PythonProject
 
+###Installation
+Récupérer le projet via git <br>
+```pip install flask```<br>
+Lancer le serveur python <br>
+```py index.py ```
+
 ###But
 Ce projet va permettre d'afficher des informations concernant le monde du cinéma au cours des années. 
 <br>Nous avons comme différentes informations : 
--- index.py
+++ index.py
@@ -39,7 +39,7 @@ def lastandnewpopulation():
     #Calcule de l'évolution
     evolution = round(((value_new - value_last)/value_last)*100,2)
     if value_last < value_new:
-        value_evolve = str(evolution) + ' % entrées'
+        value_evolve = '+'+ str(evolution) + ' % entrées'
     else: 
         value_evolve = '-'+ str(evolution) +' % entrées'
     
@@ -53,7 +53,7 @@ def lastandnewpopulation():
     #Calcule de l'évolution de la population en fonction des années
     evolution_pop = round((((new_pop - last_pop)/last_pop)*100), 2)
     if last_pop < new_pop:
-        value_evolve_pop = str(evolution_pop) + ' % de population'
+        value_evolve_pop = '+'+ str(evolution_pop) + ' % de population'
     else: 
         value_evolve_pop = '-'+ str(evolution_pop)+' % de population'
     
@@ -71,14 +71,98 @@ def lastandnewpopulation():
 
 @app.route('/fivemorelessfive/<path:year>')
 def fivemorelessfive(year):
-    results = []
-    dataminusfive = call(URL_YEAR_CINEMA + str(int(year)-5))
-    data = call(URL_YEAR_CINEMA + str(int(year)))
-    dataplusfive = call(URL_YEAR_CINEMA + str(int(year)+5))
-    results.append(json.dumps(dataminusfive['records'][0]['fields']))
-    results.append(json.dumps(data['records'][0]['fields']))
-    results.append(json.dumps(dataplusfive['records'][0]['fields']))
-    return json.dumps(results)
+    if int(year) < 2015 and int(year) > 1990: 
+        results = []
+
+        #Récupération des données de 3 années, année exact, +5 et -5
+        datamin = call(URL_YEAR_CINEMA + str(int(year)-5))
+        dataminusfive = json.dumps(datamin['records'][0]['fields'])
+        datanormal = call(URL_YEAR_CINEMA + str(int(year)))
+        data = json.dumps(datanormal ['records'][0]['fields'])
+        datamax = call(URL_YEAR_CINEMA + str(int(year)+5))
+        dataplusfive = json.dumps(datamax['records'][0]['fields'])
+
+        #Calcule des différentes données (recette)
+        #Min
+        alls_min = json.loads(dataminusfive)
+        list_key_min = list(alls_min.keys())
+        list_val_min = list(alls_min.values())
+        value_min = (list_val_min[list_key_min.index('entrees_millions')] * list_val_min[list_key_min.index('recette_moyenne_par_entree_eu')])
+        #Normal
+        alls_normal = json.loads(data)
+        list_key_normal = list(alls_normal.keys())
+        list_val_normal = list(alls_normal.values())
+        value_normal = (list_val_normal[list_key_normal.index('entrees_millions')] * list_val_normal[list_key_normal.index('recette_moyenne_par_entree_eu')])
+        #Max
+        alls_max = json.loads(dataplusfive)
+        list_key_max = list(alls_max.keys())
+        list_val_max = list(alls_max.values())
+        value_max = (list_val_max[list_key_max.index('entrees_millions')] * list_val_max[list_key_max.index('recette_moyenne_par_entree_eu')])
+        #Calcule de l'évolution des recettes
+        #Min -> Normal
+        recette_min_normal = round((((value_normal - value_min)/value_min)*100), 2)
+        if value_min < value_normal:
+            value_recette_min_normal =  '+'+ str(recette_min_normal) + ' % de recette entre '+ str(int(year)-5) + ' et '+ str(year)
+        else: 
+            value_recette_min_normal = '-'+ str(recette_min_normal)+' % de recette entre '+ str(int(year)-5) + ' et '+ str(year)
+        #Normal -> Max
+        recette_normal_max = round((((value_max - value_normal)/value_normal)*100), 2)
+        if value_normal < value_max:
+            value_recette_normal_max =  '+'+ str(recette_normal_max) + ' % de recette entre '+ str(year) + ' et '+ str(int(year)+5)
+        else: 
+            value_recette_normal_max = '-'+ str(recette_normal_max)+' % de recette entre '+ str(year) + ' et '+ str(int(year)+5)
+        #Min -> Max
+        recette_min_max = round((((value_max - value_min)/value_min)*100), 2)
+        if value_min < value_max:
+            value_recette_min_max =  '+'+ str(recette_min_max) + ' % de recette entre '+ str(int(year)-5) + ' et '+ str(int(year)+5)
+        else: 
+            value_recette_min_max = '-'+ str(recette_min_max)+' % de recette entre '+ str(int(year)-5) + ' et '+ str(int(year)+5)
+
+        #Calcule des populations en fonction des années
+        populations = call(URL_POPULATION)
+        for item in populations: 
+            if str(int(year)-5) in item:
+                pop_min = int(item[str(int(year)-5)].replace(' ',''))
+            if str(int(year)) in item:
+                pop_normal = int(item[str(int(year))].replace(' ',''))
+            if str(int(year)+5) in item:
+                pop_max = int(item[str(int(year)+5)].replace(' ',''))
+        #Calcule de l'évolution de la population en fonction des années 
+        #Min -> Normal
+        evolution_pop_min_normal = round((((pop_normal - pop_min)/pop_min)*100), 2)
+        if pop_min < pop_normal:
+            value_evolve_pop_min_normal =  '+'+ str(evolution_pop_min_normal) + ' % de population entre '+ str(int(year)-5) + ' et '+ str(year)
+        else: 
+            value_evolve_pop_min_normal = '-'+ str(evolution_pop_min_normal)+' % de population entre '+ str(int(year)-5) + ' et '+ str(year)
+        #Normal -> Max
+        evolution_pop_normal_max = round((((pop_max - pop_normal)/pop_normal)*100), 2)
+        if pop_normal < pop_max:
+            value_evolve_pop_normal_max =  '+'+ str(evolution_pop_normal_max) + ' % de population entre '+ str(year) + ' et '+ str(int(year)+5)
+        else: 
+            value_evolve_pop_normal_max = '-'+ str(evolution_pop_normal_max)+' % de population entre '+ str(year) + ' et '+ str(int(year)+5)
+        #Min -> Max
+        evolution_pop_min_max = round((((pop_max - pop_min)/pop_min)*100), 2)
+        if pop_min < pop_max:
+            value_evolve_pop_min_max =  '+'+ str(evolution_pop_min_max) + ' % de population entre '+ str(int(year)-5) + ' et '+ str(int(year)+5)
+        else: 
+            value_evolve_pop_min_max = '-'+ str(evolution_pop_min_max)+' % de population entre '+ str(int(year)-5) + ' et '+ str(int(year)+5)
+        
+        #Fabrication du tableau
+        results = { "title" : "Recette global en fonction de l'année",
+            "chart": 
+            {
+                "data":[
+                {
+                    "value" : [value_min, value_normal, value_max],
+                    "label" : "Comparaison -5 & +5"
+                }],
+                "labels": [str(int(year)-5),str(year),str(int(year)+5)]
+            },
+            "data": [value_recette_min_normal,value_evolve_pop_min_normal,value_recette_normal_max,value_evolve_pop_normal_max,value_recette_min_max,value_evolve_pop_min_max]
+        }
+        return json.dumps(results)
+    else:
+        abort(404)
 
 @app.route('/year/<path:year>')
 def index(year):
@@ -90,7 +174,7 @@ def index(year):
     list_key = list(alls.keys())
     list_val = list(alls.values())
     value = list_val[list_key.index('entrees_millions')]
-    return json.dumps(value)
+    return json.dumps(data_list)
 
 
 def call(url):
